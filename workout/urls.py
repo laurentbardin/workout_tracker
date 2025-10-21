54994cbcac5232eb7d02c71df048bb656ed27dca
Laurent Bardin <1266234-laurentbardin@users.noreply.gitlab.com>
2025-10-21 17:29:59 +0200

    Add a page to view/start the current workout

--- workout/urls.py
+++ workout/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'workout'
+
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('workout/', views.Current.as_view(), name='current'),
+    path('worksheet/<int:year>/<int:month>/<int:day>/', views.Archive.as_view(), name='archive'),
 ]
--- workout/views.py
+++ workout/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.utils import timezone
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
-from .models import Workout
+from .models import Workout, Worksheet
 
 # SQL requests reference
 # * List of exercises for the current session with their reps and weight (if
@@ -15,15 +17,51 @@ from .models import Workout
 
 # Create your views here.
 class Index(TemplateView):
+    """
+    The index displays the name of today's workout and a link to go to the
+    current worksheet.
+    """
     template_name = 'workout/index.html'
 
     def render_to_response(self, context, **response_kwargs):
-        today = timezone.now().isoweekday()
+        weekday = timezone.now().isoweekday()
         try:
-            workout = Workout.objects.get(schedule__day=today)
+            workout = Workout.objects.get(schedule__day=weekday)
         except Workout.DoesNotExist:
             workout = None
 
         context['workout'] = workout
 
         return super().render_to_response(context, **response_kwargs)
+
+class Current(TemplateView):
+    """
+    The current workout page displays the one currently in progress, or creates
+    it if it doesn't exist yet.
+    """
+    template_name = 'workout/worksheet.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        weekday = timezone.now().isoweekday()
+        try:
+            workout = Workout.objects.get(schedule__day=weekday)
+        except Workout.DoesNotExist:
+            return HttpResponseRedirect(reverse('workout:index'))
+
+        worksheet, created = Worksheet.objects.get_or_create(
+            workout=workout,
+            date=timezone.localdate(),
+        )
+
+        context['worksheet'] = worksheet
+        context['created'] = created
+
+        return super().render_to_response(context, **response_kwargs)
+
+class Archive(View):
+    """
+    Show a workout for a specific date. Using the current date should be
+    equivalent to using the 'Current' view.
+    """
+    def get(self, request, **kwargs):
+        return HttpResponse(f"Worksheet for {kwargs['year']}/{kwargs['month']}/{kwargs['day']}")
