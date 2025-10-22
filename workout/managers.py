63b0b9fe67bb1f437185f3ee8f6de91864037dbb
Laurent Bardin <1266234-laurentbardin@users.noreply.gitlab.com>
2025-10-22 17:08:35 +0200

    Improve variable name

--- workout/managers.py
+++ workout/managers.py
@@ -4,12 +4,12 @@ from django.db import models
 from django.utils import timezone
 
 class WorksheetManager(models.Manager):
-    def get_active(self, date=None):
+    def get_active(self, before=None):
         """
         Get the list of in progress workouts, if any, before a certain date. If
         none is specified, use today.
         """
-        if date is None or not isinstance(date, (datetime.datetime, datetime.date)):
-            date = timezone.now().date()
+        if before is None or not isinstance(before, (datetime.datetime, datetime.date)):
+            before = timezone.now().date()
 
-        return super().get_queryset().filter(in_progress=True, date__lt=date)
+        return super().get_queryset().filter(in_progress=True, date__lt=before)
